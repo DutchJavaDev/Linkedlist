859cbdd8be91c2ec22da912a00092309de41e92e
Boris <b.mulder@appec.nl>
2019-06-19 19:29:18 +0200

    testing item count

--- main.c
+++ main.c
@@ -5,25 +5,21 @@
 int main()
 {
     node_t * list_one;
-    node_t * list_two;
 
     create_empty_list(&list_one);
-    create_list(&list_two,5);
 
     push(&list_one,5);
 
     for(int i = 0; i < 5; i++)
     {
         push(&list_one,i*i);
-        push(&list_two,i*i*i);
     }
 
     print_nodes(list_one);
-    printf("\n");
-    print_nodes(list_two);
+    printf("\nitem count of list_one: %i\n\n",get_count(list_one));
+
 
     clear_nodes(list_one);
-    clear_nodes(list_two);
     return 0;
 }
 
