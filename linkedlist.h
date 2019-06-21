9cd117eba2da9b0c075cc5eb61b0400f948867a0
Boris <b.mulder@appec.nl>
2019-06-21 19:52:45 +0200

    cleaned up code, added exits for errors instead of keeping program alive

--- linkedlist.h
+++ linkedlist.h
@@ -50,58 +50,54 @@ node_t * create_node(int val)
 }
 
 /** Creates a linked list with the initial to be null **/
-int create_empty_list(node_t ** head)
+node_t * create_empty_list()
 {
     node_t * head_node = create_node(-1);
 
     if(head_node == NULL)
     {
         printf("Failed to create new list, malloc failed\n");
-        return FALSE;
+        exit(EXIT_FAILURE);
     }
 
     head_node->headData.initialData = FALSE;
-    *head = head_node;
-    printf("Created linked list\n");
-    return TRUE;
+    return head_node;
 }
 
 /** Creates a new linked list **/
-int create_list(node_t ** head,int initial_value)
+node_t * create_list(int initial_value)
 {
     node_t * head_node = create_node(initial_value);
 
     if(head_node == NULL)
     {
         printf("Failed to create new list, malloc failed\n");
-        return FALSE;
+        exit(EXIT_FAILURE);
     }
 
     head_node->headData.initialData = TRUE;
-    *head = head_node;
-    printf("Created linked list\n");
-    return TRUE;
+
+    return head_node;
 }
 
 /** Deletes a node **/
-int delete_node(node_t * node)
+void delete_node(node_t * node)
 {
     int node_val = node->val;
     free(node);
     node = NULL;
     printf("Node removed: val->%d\n",node_val);
-    return (node == NULL);
 }
 
 /** Prints out all the elements of the linked list **/
-int print_nodes(node_t * head)
+void print_nodes(node_t * head)
 {
+    /**TODO: this check does not work**/
     if(head == NULL)
     {
         printf("Head is null\n");
-        return FALSE;
+        exit(EXIT_FAILURE);
     }
-
 
     node_t * current = head;
     printf("\nNode values\n");
@@ -112,11 +108,10 @@ int print_nodes(node_t * head)
         current = current->next;
     }
     printf("\n");
-    return TRUE;
 }
 
 /** Free up all the memory allocated by the nodes**/
-int clear_nodes(node_t * head_node)
+void clear_nodes(node_t * head_node)
 {
     node_t * current = head_node;
     node_t * prev_node;
@@ -138,28 +133,28 @@ int clear_nodes(node_t * head_node)
             break;
         }
     }
-    return TRUE;
 }
 
 /** Removes the node item of the list **/
-int pop(node_t ** head_node)
+void pop(node_t ** head_node)
 {
     printf("Going to remove first node\n");
-    node_t * head = *head_node;
 
+    node_t * head = *head_node;
     node_t * new_head = head->next;
 
     *head_node = new_head;
 
-    return delete_node(head);
+    delete_node(head);
 }
 
 /** Removes a node at the given index **/
-int pop_at_index(node_t ** head_node,int index)
+void pop_at_index(node_t ** head_node,int index)
 {
     if(index == 0)
     {
-        return pop(head_node);
+        pop(head_node);
+        return;
     }
 
     printf("\nGoing to remove node at index: %d\n",index);
@@ -181,14 +176,15 @@ int pop_at_index(node_t ** head_node,int index)
             {
                 prev_node->next = current->next;
             }
-            return delete_node(current);
+            delete_node(current);
+            return;
         }
 
         // end of the list
         if(current->next == NULL)
         {
             printf("Node at index: %d not found\n",index);
-            return FALSE;
+            exit(EXIT_FAILURE);
         }
 
         prev_node = current;
@@ -197,11 +193,11 @@ int pop_at_index(node_t ** head_node,int index)
     }
 
     printf("Failed to remove node at index: %d\n",index);
-    return FALSE;
+    exit(EXIT_FAILURE);
 }
 
 /** Removes the last node **/
-int pop_end(node_t * head_node)
+void pop_end(node_t * head_node)
 {
     node_t * current = head_node;
     node_t * prev_node;
@@ -214,18 +210,19 @@ int pop_end(node_t * head_node)
         if(current->next == NULL)
         {
             prev_node->next = NULL;
-            return delete_node(current);;
+            delete_node(current);
+            return;
         }
 
         prev_node = current;
         current = current->next;
     }
     printf("Failed to remove last node\n");
-    return FALSE;
+    exit(EXIT_FAILURE);
 }
 
 /** Remove a node when the value equals x **/
-int pop_at_value(node_t * head_node, int value)
+void pop_at_value(node_t * head_node, int value)
 {
     node_t * current = head_node;
     node_t * prev_node;
@@ -244,13 +241,14 @@ int pop_at_value(node_t * head_node, int value)
             {
                 prev_node->next = NULL;
             }
-            return delete_node(current);
+            delete_node(current);
+            return;
         }
 
         if(current->next == NULL)
         {
             printf("Failed to find node with value: %d\n",value);
-            return FALSE;
+            exit(EXIT_FAILURE);
         }
 
         prev_node = current;
@@ -258,11 +256,11 @@ int pop_at_value(node_t * head_node, int value)
     }
 
     printf("Failed to remove node\n");
-    return FALSE;
+    exit(EXIT_FAILURE);
 }
 
 /** Add a node at the start of the node **/
-int push(node_t ** head_node, int val)
+void push(node_t ** head_node, int val)
 {
     printf("Going to add node at the begin\n");
     node_t * head = *head_node;
@@ -272,7 +270,7 @@ int push(node_t ** head_node, int val)
         head->val = val;
         head->headData.initialData = TRUE;
         printf("List was created with no initial value, %d has been placed at the head\n",val);
-        return TRUE;
+        return;
     }
 
     node_t * new_node = create_node(val);
@@ -280,21 +278,21 @@ int push(node_t ** head_node, int val)
     if(new_node == NULL)
     {
         printf("Failed to add node at begin\n");
-        return FALSE;
+        exit(EXIT_FAILURE);
     }
 
     new_node->next = head;
     *head_node = new_node;
     printf("Node added at the begin\n");
-    return TRUE;
 }
 
 /** Add a node at a given index **/
-int push_at_index(node_t ** head_node, int index, int val)
+void push_at_index(node_t ** head_node, int index, int val)
 {
     if(index == 0)
     {
-        return push(head_node,val);
+        push(head_node,val);
+        return;
     }
 
     printf("Going to insert node at index: %d\n", index);
@@ -312,7 +310,7 @@ int push_at_index(node_t ** head_node, int index, int val)
             prev_node->next = new_node;
             new_node->next = current;
             printf("Inserted node at index: %d\n",index);
-            return TRUE;
+            return;
         }
 
         // reached end of the list
@@ -321,7 +319,7 @@ int push_at_index(node_t ** head_node, int index, int val)
             node_t * new_node = create_node(val);
             current->next = new_node;
             printf("%d is bigger than list size, node added at the end\n",index);
-            return TRUE;
+            return;
         }
 
         counter++;
@@ -330,11 +328,11 @@ int push_at_index(node_t ** head_node, int index, int val)
     }
 
     printf("Failed to insert node t index: %d\n", index);
-    return FALSE;
+    exit(EXIT_FAILURE);
 }
 
 /** Adds a node at the end **/
-int push_end(node_t * head_node, int val)
+void push_end(node_t * head_node, int val)
 {
     node_t * current = head_node;
 
@@ -349,24 +347,24 @@ int push_end(node_t * head_node, int val)
             if(new_node == NULL)
             {
                 printf("Failed to add node at end\n");
-                return FALSE;
+                exit(EXIT_FAILURE);
             }
 
             current->next = new_node;
             printf("Node added at the end\n");
-            return TRUE;
+            return;
         }
         else
         {
             current = current->next;
         }
     }
-    fprintf(stderr,"Failed to add node at the end\n");
-    return FALSE;
+    printf("Failed to add node at the end\n");
+    exit(EXIT_FAILURE);
 }
 
 /** Reverses the linked list **/
-int reverse_list(node_t ** head)
+void reverse_list(node_t ** head)
 {
     node_t * next_node = (*head)->next;
     node_t * prev_node = *head;
@@ -388,7 +386,7 @@ int reverse_list(node_t ** head)
             next_node->headData = *head_data;
             *head = next_node;
             printf("List reversed\n");
-            return TRUE;
+            return;
         }
 
         /** Reversing the the connections **/
@@ -400,5 +398,5 @@ int reverse_list(node_t ** head)
     }
 
     printf("Failed to reverse list\n");
-    return FALSE;
+    exit(EXIT_FAILURE);
 }
