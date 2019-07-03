5239b677db88089815ae6e88ddd43908c41a5bca
Boris <b.mulder@appec.nl>
2019-07-03 18:51:55 +0200

    testing listmanager

--- main.c
+++ main.c
@@ -1,28 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "linkedlist.h"
+#include <string.h>
+#include "listmanager.h"
+
+
 
 int main()
 {
-    node_t * list_one;
+    NODE* intList;
+    int intListIndex;
 
-    create_empty_list(&list_one);
+    NODE* stringList;
+    int stringListIndex;
 
-    push(&list_one,5);
+    intListIndex = create_list(&intList);
+    push(&intList,465);
 
-    for(int i = 0; i < 5; i++)
+    stringListIndex = create_list(&stringList);
+    push(&stringList, "Hello LinkedList");
+    push(&stringList, "Hello World");
+
+    print_nodes_int(&intList);
+
+    printf("\n");
+
+    print_nodes_string(&stringList);
+
+    dispose();
+
+
+    /*int lenght = 3;
+    NODE** array;
+
+    /// Allocate memory for n amount of NODES
+    /// Is array, access by []
+    array = malloc(sizeof(NODE*) * lenght);
+
+    for(int i = 0; i < lenght; i++)
     {
-        push(&list_one,i*i);
+        NODE* current = &array[i];
+
+        printf("Data of node: %i\n", i);
+        if(current->nodeSignature == NODE_nodeSignature)
+        {
+            printf("same\n");
+        }
+        else
+        {
+            printf("not same\n");
+        }
+        printf("\n");
     }
 
-    print_nodes(list_one);
-    reverse_list(&list_one);
-    reverse_list(&list_one);
-    print_nodes(list_one);
-    printf("\nitem count of list_one: %i\n\n",get_count(list_one));
+    printf("\n");
 
+    /// Create NODES
+    NODE* intList;
+    create_list(&intList);
 
-    clear_nodes(list_one);
+    NODE* stringList;
+    create_node("Programming is epic", &stringList);
+
+    NODE* charList;
+    create_node_empty(&charList);
+
+    /// Fill the NODE pointer array with the addresses of the created lists
+    array[0] = &intList;
+    array[1] = &stringList;
+    array[2] = &charList;
+
+    /// Test by pushing data in to them
+    push(array[0], 15);
+    push(array[0],1337);
+
+    push(array[1], "C pointer array is hard");
+    push(array[1], "C pointers are a mess for me :/");
+
+    push(array[2], 'S');
+    push(array[2], 'I');
+    push(array[2], 'R');
+    push(array[2], 'O');
+    push(array[2], 'B');
+
+    /// Print data out using the pointer in the pointer array
+    print_nodes_int(array[0]);
+    print_nodes_string(array[1]);
+    print_nodes_char(array[2]);
+
+    /// Clear data out
+    for(int i = 0; i < lenght; i++)
+    {
+        delete_all_nodes(array[i]);
+    }
+    //delete_all_nodes(array[0]);
+    //delete_all_nodes(array[1]);
+    //delete_all_nodes(array[2]);
+
+    /// Clear pointer NODE array
+    free(array);*/
     return 0;
 }
 
